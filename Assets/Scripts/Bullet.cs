849829c99990c3655092102d80192a20078c4617
David Zhang <david8zhang@berkeley.edu>
2020-09-04 17:41:24 -0700

    initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject hitEffect;
+
+
+    private void Start()
+    {
+        Destroy(gameObject, 4f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
+    }
+}
